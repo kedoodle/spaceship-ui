fb0f6069d8551c528492f2b991650c92353c0f2a
Keddy Shen <kedd.shen@gmail.com>
2020-10-16 14:22:30 +1100

    Add header and sign out button

--- src/App.js
+++ src/App.js
@@ -4,8 +4,14 @@ import {
     Switch,
     Route
 } from "react-router-dom";
+import {
+    Button,
+    Container,
+    Typography
+} from "@material-ui/core";
 
 import Login from "./Components/Login"
+import { clearTokens } from "./Service/Auth";
 import Dashboard from "./Components/Dashboard"
 
 class App extends React.Component {
@@ -30,23 +36,36 @@ class App extends React.Component {
         })
     }
 
+    signOut = () => {
+        clearTokens();
+        this.setState({
+            authenticated: false
+        })
+    }
+
     render() {
         const authenticated = this.state.authenticated;
 
-        if (authenticated) {
-            return (
-                <Router>
-                    <Switch>
-                        <Route exact path="/">
-                            <Dashboard />
-                        </Route>
-                    </Switch>
-                </Router>
-            );
-        }
-
         return (
-            <Login setAuthenticated={this.setAuthenticated} />
+            <Container>
+                <Typography variant="h4" component="h1" gutterBottom>
+                    Spaceship UI <span aria-hidden="true" role="img">🚀</span>
+                </Typography>
+                {authenticated ?
+                    <>
+                        <Button variant="contained" onClick={this.signOut}>Sign out</Button>
+                        <Router>
+                            <Switch>
+                                <Route exact path="/">
+                                    <Dashboard />
+                                </Route>
+                            </Switch>
+                        </Router>
+                    </>
+                    :
+                    <Login setAuthenticated={this.setAuthenticated} />
+                }
+            </Container>
         );
     }
 }
